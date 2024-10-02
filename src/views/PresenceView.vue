535cb986f238bbec82865e0d5e786357f16cd141
Muhamamd Iqbal <muh.iqbalg1@gmail.com>
2024-10-03 01:04:59 +0700

    feat: Hardware FE

--- src/views/PresenceView.vue
+++ src/views/PresenceView.vue
@@ -4,7 +4,7 @@ import Camera from '@/components/LiveCameraComponent.vue';
 import DeviceName from '@/components/DeviceName.vue'
 import Modal from '@/components/Modal.vue';
 import Time from '@/components/Time.vue';
-import { ref, onMounted, effect } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import axios from 'axios';
 import VueCookies from 'vue-cookies'
 import { useForbiddenStore } from '@/stores/forbidden.js';
@@ -15,11 +15,37 @@ const ImageFrame = ref(null)
 const camera = ref(null)
 const modal = ref(false)
 const saveBtn = ref(false)
+const isCapturing = ref(false);
+const hexString = ref('');
+var is_finish = false
+var cookies
+
+
+const handleKeydown = (event) => {
+  const key = event.key;
+  if (key === 'w') {
+    if (isCapturing.value) {
+      // Log hex string when second * is pressed
+      hexString.value = hexString.value.toUpperCase().replace(/[^A-F0-9]/g, '')
+      sendImage(hexString.value)
+      isCapturing.value = false;
+      hexString.value = '';
+    } else {
+      // Start capturing after first *
+      isCapturing.value = true;
+    }
+  } else if (isCapturing.value) {
+    // Append characters to hex string if capturing
+    if (/^[0-9A-Fa-f]$/.test(key)) {
+      hexString.value += key;
+    }
+  }
+};
+
 const { show403 } = useForbiddenStore()
 const getImageFrame = (data) => {
   ImageFrame.value = data
 }
-var cookies
 const countDiff = (deltaTime) => {
   var seconds = Math.floor(deltaTime / 1000);
   var minutes = Math.floor(seconds / 60);
@@ -35,21 +61,20 @@ const convertToLocale = (time) => {
     dateStyle: 'medium'
   })
 }
-var is_finish = false
-const closeModal = () =>{
-  if(is_finish){
+const closeModal = () => {
+  if (is_finish) {
     var closeModals = setTimeout(() => {
-        modal.value.modalStatus = false
-      }, 5000)
+      modal.value.modalStatus = false
+    }, 5000)
     modal.value.closeModalObj = closeModals
-  }else{
-    setTimeout(()=>{
+  } else {
+    setTimeout(() => {
       closeModal()
     }, 2000)
   }
-  
+
 }
-const sendImage = () => {
+const sendImage = (identity = null) => {
   is_finish = false
   modal.value.data = {};
   modal.value.serverStatus.code = "pending"
@@ -59,7 +84,7 @@ const sendImage = () => {
     modal.value.data.clientPhoto = ImageFrame.value
     let data = {
       "image": ImageFrame.value,
-      "identity": idInput.value.data
+      "identity": identity ?? idInput.value.data
     }
     let config_u = {
       headers: {
@@ -67,8 +92,8 @@ const sendImage = () => {
         "Authorization": `Bearer ${cookies}`
       }
     }
-     
-    axios.post(BASE_URL+'log', data, config_u).then((response) => {
+
+    axios.post(BASE_URL + 'log', data, config_u).then((response) => {
       let tmpData = response.data.result
       let startTime = new Date(tmpData.startTime)
       let endTime = new Date(tmpData.startTime)
@@ -91,7 +116,7 @@ const sendImage = () => {
       modal.value.data.serverPhoto = BASE_URL + tmpData.serverData.image
       modal.value.data.serverBbox = tmpData.serverData.bbox
       modal.value.data.clientBbox = tmpData.clientData.bbox
-      modal.value.data.clientPhoto =  BASE_URL + tmpData.clientData.image
+      modal.value.data.clientPhoto = BASE_URL + tmpData.clientData.image
       modal.value.serverStatus = {
         'code': tmpData.isMatch ? "success" : "fail",
         'msg': tmpData.isMatch ? "Wajah Anda Cocok" : "Wajah Tidak Cocok"
@@ -103,8 +128,8 @@ const sendImage = () => {
       let errorMessage = '';
       if (error.response) {
         errorMessage = error.response.status + ': ' + error.response.data.msg
-        if (error.response.data.reson){
-            show403(error.response.data.msg)
+        if (error.response.data.reson) {
+          show403(error.response.data.msg)
         }
       } else if (error.request) {
         errorMessage = error.request.status + ': ' + error.request.statusText + error.message
@@ -112,51 +137,54 @@ const sendImage = () => {
       } else {
         errorMessage = error.message
       }
-      modal.value.serverStatus = { 'code': "fail", 'msg': errorMessage, "status": error.response ? error.response.status ?? error.request.status : 0};
+      modal.value.serverStatus = { 'code': "fail", 'msg': errorMessage, "status": error.response ? error.response.status ?? error.request.status : 0 };
     });
     closeModal()
   }, 200)
 }
 onMounted(
   () => {
+    document.addEventListener('keydown', handleKeydown);
     cookies = VueCookies.get('device_token');
     if (!cookies) {
       show403("Device Not Registered")
     }
     socket.on('reload-user-page', () => {
-    window.location.reload();
-  });
+      window.location.reload();
+    });
   }
 )
-effect(() => {
-  if (saveBtn.value) {
-    if (idInput.value.data.length > 4) {
-      saveBtn.value.disabled = false
-    } else {
-      saveBtn.value.disabled = true
-    }
-    if(idInput.value.data.indexOf('*') > 0 ){
-      sendImage()
-    }
-    idInput.value.data = idInput.value.data.replace(/[^A-F0-9]/g, '')
+watch(() => idInput.value.data.length, (newVal) => {
+  idInput.value.data = idInput.value.data.replace(/[^0-9]/g, '')
+  let saveButtonObj = saveBtn.value.disabled
+  saveButtonObj = false
+  if (newVal < 5) {
+    saveButtonObj = false
   }
 })
 </script>
 
 <template>
   <main class="flex items-center justify-center w-full relative h-screen">
-    <div class="w-full max-w-[1200px] flex flex-col justify-center items-center">
+    <div class="w-full max-w-[1200px] flex flex-col justify-center items-center ">
       <TitleComponent title="UNNESTech" subtitle="Presence" />
       <Modal ref="modal" />
-      <div class="flex items-center justify-center w-full drop-shadow-xl bg-gray-100 mt-8 p-2 pr-10 rounded-2xl">
-        <div class="w-[500px]">
+      <div class="flex items-center flex-col md:flex-row justify-center w-full drop-shadow-xl bg-gray-100 mt-8 p-2 md:pr-10 rounded-2xl py-5">
+        <div class="md:hidden text-3xl font-bold mt-4">
+          <img src="/src/assets/logo/logo-unnes-horizontal.png" alt="" style="width: 350px; height: auto" class="invert" />
+        </div>
+        <div class="w-[700px] md:w-[500px]">
           <Camera @image-frame="getImageFrame" ref="camera" />
         </div>
-        <div class="ml-4 flex flex-col items-start justify-center">
-          <DeviceName/>
-          <Time/>
-          <label for="identitas" class="text-2xl mt-2"><i class="pi pi-id-card"></i> Indentitas (NIM/NIP)</label>
-          <input v-on:keyup.enter="sendImage" type="text" v-model="idInput.data" class="w-full mt-2 text-2xl rounded-md border border-black px-3">
+        <div class="ml-4 flex flex-col items-center md:items-start
+         justify-center">
+         <div class="mb-2 md:mb-1">
+           <DeviceName />
+         </div>
+          <Time />
+          <label for="identitas" class="text-3xl md:text-2xl mt-2"><i class="pi pi-id-card"></i> Indentitas (NIM/NIP)</label>
+          <input v-on:keyup.enter="sendImage" type="text" v-model="idInput.data"
+            class="w-full mt-2 text-2xl rounded-md border border-black px-3">
           <button @click="sendImage" disabled="true" ref="saveBtn"
             class="bg-blue-700 transition duration-300 text-white py-2 mt-2 px-3 rounded-lg disabled:bg-gray-500  hover:bg-blue-900 hover:drop-shadow-lg ">
             <i class="pi pi-save mr-2"></i>Simpan</button>
