c1e9ec11c92c1294b9dc182c53cb5b488a71db38
Mousynano <104660173+Mousynano@users.noreply.github.com>
2024-11-28 13:24:51 +0700

    feat: added doorlock views

---
+++ src/services/Doorlock.services.js
@@ -0,0 +1,3 @@
+export const goToDoorlock = () => {
+  window.location.href = '/doorlock' // Ganti dengan route Anda jika menggunakan Vue Router
+}
---
+++ src/services/Presence.services.js
@@ -0,0 +1,3 @@
+export const goToPresence = () => {
+  window.location.href = '/presence' // Ganti dengan route Anda jika menggunakan Vue Router
+}
