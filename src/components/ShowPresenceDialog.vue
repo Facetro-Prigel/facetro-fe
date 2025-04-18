21376be43239d09d63ac2b58553c75c8eac7c97c
pc2 <muh.iqbalg1@gmail.com>
2025-04-18 21:26:13 +0700

    fix: log per user remining while only login

--- src/components/ShowPresenceDialog.vue
+++ src/components/ShowPresenceDialog.vue
@@ -358,8 +358,47 @@ watch(() => props.visible, async (newVal) => {
         },
       ],
     };
+    startUpdatingTime();
   }
 })
+const updateChartData = (updatedDailyMinutes) => {
+  const minutes = updatedDailyMinutes.map((item) => item.minutes);
+
+  // Fungsi untuk menentukan warna latar belakang
+  const getBackgroundColor = (minutes) => {
+    return minutes > 480 ? 'rgba(50, 205, 50,.75)' : 'rgba(199, 0, 57, .75)';
+  };
+
+  const backgroundColors = minutes.map((minute) => getBackgroundColor(minute));
+
+  // Perbarui chartData
+  chartData.value = {
+    labels: dates,
+    datasets: [
+      {
+        type: 'line',
+        label: 'Waktu yang dihabiskan',
+        data: minutes,
+        borderColor: 'orange',
+        backgroundColor: backgroundColors,
+        tension: 0.5,
+      },
+      {
+        type: 'bar',
+        label: 'Waktu yang dihabiskan',
+        data: minutes,
+        backgroundColor: backgroundColors,
+      },
+    ],
+  };
+};
+const getCurrentDate = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Bulan dimulai dari 0
+  const day = String(now.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
 const convertToLocale = (time) => {
   return time.toLocaleString('id-ID', {
     timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
