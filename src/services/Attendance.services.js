3b73917b2fe958c30443bb46cfcf80878f6ca472
muhammadhaniffadhlurrahman24 <muhammadhaniffadhlurrahman24@gmail.com>
2025-05-27 12:50:34 +0700

    feat : add quickfullreport at attendance

--- src/services/Attendance.services.js
+++ src/services/Attendance.services.js
@@ -11,9 +11,33 @@ export const fetchAttendanceLogs = async (data) => {
   }
 };
 
-export const downlaodAttendanceLogs = async (data) => {
+export const downlaodQuickAttendanceLogs = async (data) => {
   try {
-    const response = await apiClient.get(`/log/recap?start_date=${data.start_date}&end_date=${data.end_date}`);
+    const columns = data.columns.join(',')
+    const response = await apiClient.get(`/log/quick_recap?start_date=${data.start_date}&end_date=${data.end_date}&columns=${columns}`);
+
+    console.log(JSON.stringify(response.data))
+    const base64Data = response.file; // Pastikan ini sesuai dengan respons API sebenarnya
+    const url = base64Data;
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", `Laporan ${convertToLocale(new Date())}.xlsx`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error('Ada kesalahan saat mengunduh file!', error);
+    throw error;
+  }
+};
+
+export const downlaodFullAttendanceLogs = async (data) => {
+  try {
+    console.log(JSON.stringify(data))
+    const response = await apiClient.get(`/log/full_recap?start_date=${data.start_date}&end_date=${data.end_date}`);
+    console.log(JSON.stringify(response))
 
     const base64Data = response.file; // Pastikan ini sesuai dengan respons API sebenarnya
     const url = base64Data;
